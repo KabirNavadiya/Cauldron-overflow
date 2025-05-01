be9df68aff798307d70423e47608e309a3b60592
KabirNavadiya <kabirnavadia27@gmail.com>
2025-05-01 18:05:36 +0530

    covered topics like queries,twig,ago,query-builder,query-reuse,votes,request,update

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -6,6 +6,7 @@ use App\Entity\Question;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -47,23 +48,30 @@ class QuestionRepository extends ServiceEntityRepository
         }
     }
 
-    // /**
-    //  * @return Question[] Returns an array of Question objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return Question[]  Returns an array of Question objects
+     */
+
+    public function findAllAskedOrderedByNewest()
     {
-        return $this->createQueryBuilder('q')
-            ->andWhere('q.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('q.id', 'ASC')
-            ->setMaxResults(10)
+        return $this->addIsAskedQueryBuilder()
+            ->orderBy('q.askedAt','DESC')
             ->getQuery()
             ->getResult()
         ;
     }
-    */
 
+    private function addIsAskedQueryBuilder(QueryBuilder $qb=null): QueryBuilder
+    {   
+        return $this->getOrCreateQueryBuilder($qb)
+            ->andWhere('q.askedAt IS NOT NULL'); 
+    }
+
+
+    private function getOrCreateQueryBuilder(QueryBuilder $qb = null): QueryBuilder
+    {
+        return $qb ?: $this->createQueryBuilder('q');
+    }
     /*
     public function findOneBySomeField($value): ?Question
     {
