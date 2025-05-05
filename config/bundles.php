648c3b3265e4e2ca62e1822735c51148f2bf938f
KabirNavadiya <kabirnavadia27@gmail.com>
2025-05-05 15:05:39 +0530

    fixtures,foundry,sluggable

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -35,44 +35,16 @@ class QuestionController extends AbstractController
         return $this->render('question/homepage.html.twig', [
             'questions' => $questions,
         ]);
-    }
+    } 
 
     /**
      * @Route("/questions/newquestion")
      */
     public function newquestion(EntityManagerInterface $entitymanager)
     {
-        $question = new Question();
-        $question->setName('Missing pants')
-            ->setSlug('missing-pants' . rand(0, 1000))
-            ->setQuestion(
-                "
-            Hi! So... I'm having a *weird* day. yesterday, I cast a spell
-            to make my dishes wash themselves. But while I was casting it,
-            I slipped a little and I think `I also hit my pants with the spell`.
+       
 
-            When I woke up this morning, I caught a quick glimpse of my pants
-            opening the front door and walking out! I've been out all afternoon
-            (with no pants mind you) searching for them.
-
-            Does anyone have a spell to call your pants back?
-            "
-            );
-        if (rand(1, 10) > 2) {
-            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
-        }
-
-
-        $question->setVotes(rand(-20, 50));
-
-        $entitymanager->persist($question);
-        $entitymanager->flush();
-
-        return new Response(sprintf(
-            'well hello! The shiny new question is id #%d, slug %s',
-            $question->getId(),
-            $question->getSlug()
-        ));
+        return new Response('Sounds like a GREAT feature for V2');
     }
 
     /**
