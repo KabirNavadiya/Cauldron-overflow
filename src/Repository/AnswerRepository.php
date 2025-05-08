20b75333c74f7d02ee27a43e0aca023be6f54367
KabirNavadiya <kabirnavadia27@gmail.com>
2025-05-08 17:39:59 +0530

    implementd search-form

--- src/Repository/AnswerRepository.php
+++ src/Repository/AnswerRepository.php
@@ -70,16 +70,23 @@ class AnswerRepository extends ServiceEntityRepository
     /**
      * @return Answer[]
      */
-    public function findMostPopular():array
+    public function findMostPopular(string $search = null):array
     {
-        return $this->createQueryBuilder('answer')
+        $queryBuilder = $this->createQueryBuilder('answer')
                         ->addCriteria(self::createApprovedCriteria())
                         ->orderBy('answer.votes','DESC')
                         ->innerJoin('answer.question','question')
-                        ->addSelect('question')
-                        ->setMaxResults(10)
-                        ->getQuery()
-                        ->getResult();
+                        ->addSelect('question');
+
+        if($search){
+            $queryBuilder->andWhere('answer.content LIKE :searchTerm OR question.question LIKE :searchTerm') 
+                        ->setParameter('searchTerm','%'.$search.'%');
+        }
+
+        return $queryBuilder
+                ->setMaxResults(10)
+                ->getQuery()
+                ->getResult();
     }
 
 
