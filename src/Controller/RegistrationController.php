e3d1742bc3b9d626fa56a70a948cbc3d9839c738
KabirNavadiya <kabirnavadia27@gmail.com>
2025-05-14 18:32:51 +0530

    verify url,  verify-signed-url

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
 use App\Security\LoginFormAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,13 +14,15 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $loginFormAuthenticator): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, VerifyEmailHelperInterface $verifyEmailHelper): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -34,17 +37,60 @@ class RegistrationController extends AbstractController
                 )
             );
 
+
+
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-           return $userAuthenticator->authenticateUser($user, $loginFormAuthenticator,$request);
+            $signatureComponents = $verifyEmailHelper->generateSignature(
+                'app_verify_email',
+                $user->getId(),
+                $user->getEmail(),
+                ['id'=>$user->getId()]
+            );
 
-            // return $this->redirectToRoute('app_homepage');
+            //TODO: in real app, send this as an email.
+            $this->addFlash('success','Confirm your email at '.$signatureComponents->getSignedUrl());
+
+            return $this->redirectToRoute('app_homepage');
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    /**
+     * 
+     * @Route("/verify",name="app_verify_email")
+     */
+    public function verifyuserEmail(Request $request, VerifyEmailHelperInterface $verifyEmailHelper, UserRepository $userRepository, EntityManagerInterface $entityManager)
+    {
+        $user= $userRepository->find($request->query->get('id'));
+
+        if(!$user){
+            throw $this->createNotFoundException();
+        }
+
+        try {
+            $verifyEmailHelper->validateEmailConfirmation(
+                $request->getUri(),
+                $user->getId(),
+                $user->getEmail(),
+                 
+            );
+        } catch (VerifyEmailExceptionInterface $e) {
+            $this->addFlash('error',$e->getReason());
+
+            return $this->redirectToRoute('app_register');
+        }
+
+       $user->setIsVerified(true);
+
+       $entityManager->flush();
+
+       $this->addFlash('success','Account verified. Now you can log in. ');
+       return $this->redirectToRoute('app_login');
     }
 }
